11ed693eca29a2879341996aa17c03e681b5eff8
Romina <bidehromina86@gmail.com>
2021-12-28 18:49:30 +0000

    iiiihhfffjjk

---
+++ gitPractice/1-gitHomework.js
@@ -0,0 +1,30 @@
+//? practice to undrestand difference between const and let.
+//const and let are variable, on the other hand const is better than let because const variable won't be reasigned however let could be reasigned.
+// In conclusion:
+// "Var" variable can be updated and re-declared.don't need initialized.
+// "let" variable can be updated but not re-declared. don't need initialized.
+// "const" variable neither be update and re-declared. needs initialized.
+
+//?practice git and read the codeyourfuture git syllabus.
+//I read the git from codeYourFuture syllabus and also I watched the https://www.youtube.com/watch?v=BCQHnlnPusY and https://www.youtube.com/watch?v=iv8rSLsi1xo
+// Now I know how I can make a repo and how I can commit and add more commit, eventhough we could do the same in VS, git and gitHub.
+// In conclusion I need more practice to pull and push and make changes on it to clear all the options.
+
+//? git push, git pull, what is stage area in git, what is remote repository in git
+//? What is differece between local repository and remote repo in github.
+//"gitPush"
+//"gitPull"
+//"StageArea"
+//"remoteRepository": is a temporary place to put our files to the up and 
+
+//? cheat sheet git find it.
+
+//?different between git and github
+
+//? fix linkD profile.
+
+//? search about stack overflow debogger platform
+
+//? codebar
+
+//? imporvement lestweek
