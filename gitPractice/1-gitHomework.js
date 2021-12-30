ad0531ae35945760df4b6dc7c5555c5adef40d32
Romina <bidehromina86@gmail.com>
2021-12-30 14:52:31 +0000

    I answer the questions.

--- gitPractice/1-gitHomework.js
+++ gitPractice/1-gitHomework.js
@@ -12,19 +12,34 @@
 
 //? git push, git pull, what is stage area in git, what is remote repository in git
 //? What is differece between local repository and remote repo in github.
-//"gitPush"
-//"gitPull"
-//"StageArea"
-//"remoteRepository": is a temporary place to put our files to the up and 
+//"gitPull": If we want to update our branchs with our team mate, we need to write gitpull in terminal to check what have changed, on the other hand we could use git adn github.
+//"StageArea": is a temporary place to put our files to the up. in source control We need to click on + for each files we feel it could be finished, we could press the +.
+//"gitPush": If we want to send our changes in branch, we can write gitpush in terminal and also we could use git or github to update our branches.
+//"remoteRepository" I saw https://www.youtube.com/watch?v=lR_hYwCAaH4&list=PLRqwX-V7Uu6ZF9C0YMKuns9sLDzK6zoiV&index=10
 
-//? cheat sheet git find it.
+
+
+//? git cheatsheet -> find it.
+//Git CLI cheatsheet. This is a terminal which we could write rather than use git. However we could see the changes on the git as well.
+//"Terminal":
+//Using the terminal navigate to a specified folder (using cd)
+//Move up a level in a folder directory (using cd ..)
+//Cancel a running task in the terminal (using ctrl+c)
+// I could not find Windows WSL. I know that it is for linux.
 
 //?different between git and github
+//This is the difference between Git and GitHub. Git is a way of storing and sharing files, and GitHub is one website where you can use Git.
 
-//? fix linkD profile.
+
 
 //? search about stack overflow debogger platform
+// I could find any problem or any question about everything in coding and solve my problems.
+// The most important overflow debugger is MDN.
 
 //? codebar
+//I could ask my questions and aloso joint to the for any workshop what I am interested. 
+
+//? fix linkD profile.
+// I wrote more about myself in linkdin and explaned what I did for my clients and office.
 
 //? imporvement lestweek
